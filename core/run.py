05d1afaa8ef21637f891c2dfed6d63697c802a31
Ash Kinzy <kxnzyth0@gmail.com>
2018-04-25 21:27:33 +0100

    Pre-game messages and inputs

--- core/run.py
+++ core/run.py
@@ -1,13 +1,48 @@
+import time
+
+import characters as chars
+from entity import Character
+from game import Game
+
+
+def wait():
+    time.sleep(0)
+
+
+def pick_character():
+    lines = [f"#{c['id']} - {c['name']} - {c['desc']}" for c in chars.all]
+    print("\n".join(lines))
+
+    choice = input("\n")
+
+    if choice not in [c["name"] for c in chars.all]:
+        print("Invalid character, please choose a name from the list.\n")
+        pick_character()
+    else:
+        for char in chars.all:
+            if char["name"] == choice:
+                return Character(**char)
+
+
 def main():
-    print("-------------------------------------\n"
-          "|                                   |\n"
-          "|         Welcome to Aesir!         |\n"
-          "|                                   |\n"
-          "| Aesir, a text based adventure     |\n"
-          "| game based on Norse Mythology,    |\n"
-          "| created by Folkvangr Studios.     |\n"
-          "|                                   |\n"
-          "-------------------------------------")
+    print(("-------------------------------------\n"
+           "|                                   |\n"
+           "|         Welcome to Aesir!         |\n"
+           "|                                   |\n"
+           "-------------------------------------\n"))
+    wait()
+
+    name = input("Let's begin, firstly, what is your name?\n")
+    print(f"Great, so your name is {name}.\n")
+    wait()
+
+    print("Before you can begin playing, you must chose a character.")
+    print("Please select one of the relevant characters from below.\n")
+
+    character = pick_character()
+
+    game = Game(char=character)
+    game.start()
 
 
 if __name__ == "__main__":
