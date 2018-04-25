5144b7eeb88236410e5a7b404f2e8929f094b322
Ash Kinzy <kxnzyth0@gmail.com>
2018-04-25 21:27:51 +0100

    Characters.py for all chars

---
+++ core/characters.py
@@ -0,0 +1,13 @@
+bert = {
+    "id": 1,
+    "name": "bert",
+    "desc": "good attacker",
+    "hp": 10,
+    "attack": 10,
+    "defence": 10,
+    "magic": 10,
+    "speed": 10
+}
+
+
+all = (bert,)
