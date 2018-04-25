2d0ccb6bf236ff27f2102c575ea7f6faf8081f97
Ash Kinzy <kxnzyth0@gmail.com>
2018-04-25 21:26:22 +0100

    Tidied up args

--- core/entity.py
+++ core/entity.py
@@ -1,18 +1,20 @@
 class Entity(object):
     """The base class that all characters/mobs follow."""
-    pass
+
+    def __init__(self, *args, **kwargs):
+        self.name = kwargs.pop("name")
 
 
 class Character(Entity):
-    def __init__(self, hp, attack, defence, magic, speed, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # Stats
-        self.hp = hp
-        self.attack = attack
-        self.defence = defence
-        self.magic = magic
-        self.speed = speed
+        self.hp = kwargs.pop("hp")
+        self.attack = kwargs.pop("attack")
+        self.defence = kwargs.pop("defence")
+        self.magic = kwargs.pop("magic")
+        self.speed = kwargs.pop("speed")
 
         self.attack_points = self.attack * 4
         self.magic_points = self.magic * 3
