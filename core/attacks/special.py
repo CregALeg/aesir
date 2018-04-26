2e4953c3f20baf7d0120c74e404bc630e2a9f43f
CregALeg <30729125+CregALeg@users.noreply.github.com>
2018-04-26 10:14:53 +0100

    Create special.py

---
+++ core/attacks/special.py
@@ -0,0 +1,34 @@
+axe_toss = {
+  "name":"Axe Toss",
+  "power":6,
+  "attack_points": 8,
+  "effect":"stun"
+}
+
+shield_bash = {
+  "name":"Shield Bash",
+  "power":4,
+  "attack_points": 5,
+  "effect":"fortify"
+}
+
+quick_stab = {
+  "name":"Quick Stab",
+  "power":5,
+  "attack_points": 6,
+  "effect":"priority"
+}
+
+shoulder_bash = {
+  "name":"Shoulder Bash",
+  "power":7,
+  "attack_points": 6,
+  "effect":"slow"
+}
+
+guard_break = {
+  "name":"Guard Break",
+  "power":3,
+  "attack_points": 4,
+  "effect":"expose"
+}
