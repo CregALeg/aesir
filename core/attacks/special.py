37a41c34be01a81c13be7a2d5e4b096e63d7970c
CregALeg <30729125+CregALeg@users.noreply.github.com>
2018-04-26 10:39:51 +0100

    Added new attacks

--- core/attacks/special.py
+++ core/attacks/special.py
@@ -12,11 +12,32 @@ shield_bash = {
   "effect":"fortify"
 }
 
+battle_cry = {
+  "name":"Battle Cry",
+  "power":0,
+  "attack_points": 6,
+  "effect":"empower"
+}
+
 quick_stab = {
   "name":"Quick Stab",
   "power":5,
   "attack_points": 6,
   "effect":"priority"
+}
+
+bow_shot = {
+  "name":"Bow Shot",
+  "power":3,
+  "attack_points": 10,
+  "effect":"headshot"
+}
+
+poison_dagger = {
+  "name":"Poison Dagger",
+  "power":3,
+  "attack_points": 8,
+  "effect":"poison"
 }
 
 shoulder_bash = {
@@ -32,3 +53,12 @@ guard_break = {
   "attack_points": 4,
   "effect":"expose"
 }
+
+hammer_smash = {
+  "name":"Hammer Smash",
+  "power":8,
+  "attack_points": 10,
+  "effect":"weaken"
+}
+
+all = (axe_toss, shield_bash, battle_cry, quick_stab, bow_shot, poison_dagger, shoulder_bash, guard_break, hammer_smash)
